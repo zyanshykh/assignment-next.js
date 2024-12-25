c770b50b40bae7d91bebb3d1a24157e6549129f9
Zayan-Sheikh <160139662+zyanshykh@users.noreply.github.com>
2024-12-25 22:35:30 +0500

    nextjs-hello-world

---
+++ components/About.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return(
+        <section>
+            <h3>About This is First-Time!</h3>
+            <p>This is a simple Next.js Application using Typescript. </p>
+        </section>
+    );
+};
+export default About;
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+const Header = ()=>{
+    return(
+        <header>
+            <h2>My Next.js App First-Time</h2>
+        </header>
+    );
+};
+export default Header;
